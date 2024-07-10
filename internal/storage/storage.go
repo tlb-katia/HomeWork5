6678383b6c53989aaaf3252959e30e21929a88c1
katia <tlb_kei7@yandex.ru>
2024-07-10 19:12:58 +0300

    1

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,1 +1,50 @@
 package storage
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"os"
+)
+
+func NewDB() (*sql.DB, error) {
+	const op = "storage.NewDB"
+
+	err := godotenv.Load()
+	if err != nil {
+		return nil, fmt.Errorf("%s %w", op, err)
+	}
+
+	user := os.Getenv("DB_USER")
+	dbname := os.Getenv("DB_NAME")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return nil, fmt.Errorf("%s %w", op, err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("%s %w", op, err)
+	}
+
+	return db, nil
+}
+
+func CloseDB(db *sql.DB) error {
+	const op = "storage.CloseDB"
+
+	err := db.Close()
+	if err != nil {
+		return fmt.Errorf("%s %w", op, err)
+	}
+
+	return nil
+}
--- internal/user/user.go
+++ internal/user/user.go
@@ -1,1 +1,40 @@
 package user
+
+import (
+	"context"
+)
+
+type User struct {
+	ID       int64  `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type UserReq struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type UserRes struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+type LoginUser struct {
+	Token    string `json:"token"`
+	Username string `json:"username"`
+	ID       int64  `json:"id"`
+}
+
+type Repository interface {
+	CreateUser(ctx context.Context, user *User) (*User, error)
+	GetUserByEmail(ctx context.Context, email string) (*User, error)
+}
+
+type Service interface {
+	CreateUser(ctx context.Context, user *UserReq) (*UserRes, error)
+	Login(ctx context.Context, user *UserReq) (*LoginUser, error)
+}
